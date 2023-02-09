288da086a5a736c1ca087d1d21892d5e78263130
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2023-02-10 01:12:52 +0900

    move components

--- src/Graphics.svelte
+++ src/Graphics.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import GameArea from './lib/GameArea.svelte'
-  import Sidebar from './lib/Sidebar.svelte'
+  import GameArea from './lib/graphics/GameArea.svelte'
+  import Sidebar from './lib/graphics/Sidebar.svelte'
 
   export let mirrorId
 </script>
