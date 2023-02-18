4cf10f67d4d25b165666634bbdc81cf8f69ff04e
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2023-02-18 23:49:18 +0900

    separate timer parts to new component

--- src/browser/dashboard/GroupControl.svelte
+++ src/browser/dashboard/GroupControl.svelte
@@ -2,22 +2,11 @@
   import type { Group } from '~/types/group'
   import { timerFor } from '../store/timer'
   import RunnerControl from './lib/RunnerControl.svelte'
+  import TimeControl from './lib/TimeControl.svelte'
 
   export let mirrorId: Group
   $: timer = timerFor(mirrorId)
 </script>
 
 <RunnerControl {mirrorId} />
-<div>
-  <p>残り時間: {$timer?.format()}</p>
-  <!--
-            <button id="resetButton" onclick="timerReset();">リセット</button>
-            <button id="startButton" onclick="timerStart();" disabled>スタート</button>
-            <button id="stopButton" onclick="timerStop();" disabled>ストップ</button>
-            <button id="successButton" onclick="timerSuccess();" disabled>記録達成</button>
-            <br>
-            <button id="reloadButton" onclick="reload();">更新</button>
-            <button id="setupButton" onclick="setupRunner();">準備中にする</button>
-            <button id="finishButton" onclick="finishRunner();">終了にする</button>
--->
-</div>
+<TimeControl {mirrorId} />
---
+++ src/browser/dashboard/lib/TimeControl.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { timerFor } from '~/browser/store/timer'
+  import type { Group } from '~/types/group'
+
+  export let mirrorId: Group
+  $: timer = timerFor(mirrorId)
+</script>
+
+<div>
+  <p id="subject">タイマー</p>
+  <p>残り時間: {$timer?.format()}</p>
+  <!--
+            <button id="resetButton" onclick="timerReset();">リセット</button>
+            <button id="startButton" onclick="timerStart();" disabled>スタート</button>
+            <button id="stopButton" onclick="timerStop();" disabled>ストップ</button>
+            <button id="successButton" onclick="timerSuccess();" disabled>記録達成</button>
+            <br>
+            <button id="reloadButton" onclick="reload();">更新</button>
+            <button id="setupButton" onclick="setupRunner();">準備中にする</button>
+            <button id="finishButton" onclick="finishRunner();">終了にする</button>
+-->
+</div>
+
+<style scoped>
+  button {
+    margin-bottom: 0.2em;
+  }
+
+  img {
+    display: block;
+    width: 50px;
+  }
+
+  #subject {
+    font-size: 1.6em;
+    font-weight: bold;
+  }
+
+  p {
+    margin: 0.2em 0;
+  }
+</style>
