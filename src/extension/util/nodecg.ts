ea3c57c76085473e890b3618bf719a6ca01ccf94
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2023-02-18 21:51:19 +0900

    add utility for global NodeCG instance

---
+++ src/extension/util/nodecg.ts
@@ -0,0 +1,13 @@
+import type { NodeCG } from '../nodecg'
+
+let instance: NodeCG
+
+function get() {
+  return instance
+}
+
+function set(instance: NodeCG) {
+  return instance
+}
+
+export { get, set }
