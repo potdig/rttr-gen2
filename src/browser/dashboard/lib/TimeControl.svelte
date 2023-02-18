5a52b06cc5d9742d8de0cef3cb7d368c41a7ec1b
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2023-02-19 00:25:05 +0900

    add button for timer control

--- src/browser/dashboard/lib/TimeControl.svelte
+++ src/browser/dashboard/lib/TimeControl.svelte
@@ -1,42 +1,69 @@
 <script lang="ts">
   import { timerFor } from '~/browser/store/timer'
   import type { Group } from '~/types/group'
+  import type { TimerState } from '~/types/timer'
 
   export let mirrorId: Group
   $: timer = timerFor(mirrorId)
+
+  function start() {
+    manipulate('Start')
+  }
+
+  function pause() {
+    manipulate('Pause')
+  }
+
+  function reset() {
+    manipulate('Stop')
+  }
+
+  function success() {
+    manipulate('Success')
+  }
+
+  function manipulate(state: TimerState) {
+    window.nodecg.sendMessage('manipulateTimer', {
+      group: mirrorId,
+      state,
+    })
+  }
 </script>
 
-<div>
+<div id="timer-control">
   <p id="subject">タイマー</p>
   <p>残り時間: {$timer?.format()}</p>
-  <!--
-            <button id="resetButton" onclick="timerReset();">リセット</button>
-            <button id="startButton" onclick="timerStart();" disabled>スタート</button>
-            <button id="stopButton" onclick="timerStop();" disabled>ストップ</button>
-            <button id="successButton" onclick="timerSuccess();" disabled>記録達成</button>
+  <div id="buttons">
+    <button on:click={start}>スタート</button>
+    <button on:click={pause}>ストップ</button>
+    <button on:click={reset}>リセット</button>
+    <button on:click={success}>記録達成</button>
+    <!--
             <br>
             <button id="reloadButton" onclick="reload();">更新</button>
             <button id="setupButton" onclick="setupRunner();">準備中にする</button>
             <button id="finishButton" onclick="finishRunner();">終了にする</button>
 -->
+  </div>
 </div>
 
 <style scoped>
-  button {
-    margin-bottom: 0.2em;
+  #timer-control {
+    width: 100%;
   }
 
-  img {
-    display: block;
-    width: 50px;
+  p {
+    margin: 0.2em 0;
+  }
+
+  #buttons {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 0.2em;
   }
 
   #subject {
     font-size: 1.6em;
     font-weight: bold;
-  }
-
-  p {
-    margin: 0.2em 0;
   }
 </style>
